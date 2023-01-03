8d1ecd427001ecc78ba7eaca7e05d7753d141904
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2023-01-03 23:21:56 +0200

    Added app.get

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
 app.listen(3000, () => {
     console.log('running on port 3000')
