c11dc241c9ce410c5c1697e99fcfb59d99119c30
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2023-01-22 15:43:36 +0200

    Added passport packages

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const req = require('express/lib/request');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -39,39 +40,11 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
 
-        newUser.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('secrets');
-            }
-        });
-    });
 });
 
 app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({ email: username }, (err, foundUser) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                 bcrypt.compare(password, foundUser.password, (err, result) => {
-                    if (result === true) {
-                        res.render('secrets');
-                    }
-                });
-            }
-        }
-    });
 });
 
 app.listen(3000, () => {
