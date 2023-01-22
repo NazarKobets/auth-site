20f4c28e58f4d312f14f2d281ee412b1d0ab7b5f
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2023-01-22 16:28:05 +0200

    Added new encryotion method into /login

--- app.js
+++ app.js
@@ -77,7 +77,20 @@ app.post('/register', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
 
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets');
+            });
+        }
+    })
 });
 
 app.listen(3000, () => {
