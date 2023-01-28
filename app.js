73d92c0c7b95818839c5fde5a0823793a9a48d0d
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2023-01-28 20:48:50 +0200

    Added mongoose-findorcreate

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
